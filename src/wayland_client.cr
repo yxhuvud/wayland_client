9e4a8c5106031448ff9b62c547ec9ae2ad2ce5b8
Linus Sellberg <sellberg@gmail.com>
2022-12-28 18:53:06 +0100

    wayland client: Add comments to example
    A lot of these should be copied over to the method definitions as well.

--- src/wayland_client.cr
+++ src/wayland_client.cr
@@ -33,9 +33,11 @@ WaylandClient.display do |display|
     buffer_pool: WaylandClient::Buffer.new(:memory, WaylandClient::Format::XRGB8888),
     opaque: true
   )
-  # fixme
-  subsurface = surface.create_subsurface sync: false, opaque: true
-
+  # Creates an async subsurface, as there is (currently) no way to use
+  # libdecor with async top surfaces. An async surface is necessary or
+  # else there will be update events only when the outer frame wake up
+  # and wants to update. Which won't be very often.
+  subsurface = surface.create_subsurface(sync: false, opaque: true)
 
   frame_callback = Proc(UInt32, Nil).new do |time|
     frame_counter.register time
@@ -43,13 +45,28 @@ WaylandClient.display do |display|
     subsurface.surface.repaint!(&.map! { WHITE })
   end
 
+  # The block is called on initialization and on resize, and
+  # potentially in more cases.
   display.create_frame(surface, title: "hello", app_id: "hello app") do |x, y, window_state|
     setup_counter.register
 
-    surface.repaint!(flush: false, &.set_all { |x1, y1| BLACK })
+    # The base surface needs an attached buffer, or there will be no
+    # window generated, but it actually don't need to be painted as
+    # the subsurface will cover the whole of it.
+    surface.attach_buffer
+    surface.commit
+
+    # Make sure to paint the subsurface here as it will look wrong on
+    # resize otherwise.
     subsurface.surface.resize(*surface.size)
     subsurface.surface.repaint! &.map! { WHITE }
 
+    # Automatically generate new frames at monitor FPS. Cannot be set
+    # up before configuration (ie this block) has been run, but also
+    # has to be regenerated on each configuration due to what I
+    # believe is a compositor bug. If it is only set up once it will
+    # stop running if there is a configuration event, but if a new
+    # frame is requested it will run both!
     subsurface.surface.request_frame(frame_callback)
   end
 
