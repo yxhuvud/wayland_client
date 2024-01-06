64558e9396cb96d86e0402ada85379efbea34a3e
Linus Sellberg <linus.sellberg@nj.se>
2024-01-06 11:47:32 +0100

    Buffer::Memory: More refactoring

--- src/buffer/memory.cr
+++ src/buffer/memory.cr
@@ -13,12 +13,14 @@ module WaylandClient
       getter fd, registry, pool, x_size, y_size
 
       def initialize(@registry : WaylandClient::Registry, @pool : Pool(Memory(T)))
-        @fd = IO::FileDescriptor.new(LibC.memfd_create("buffer".to_unsafe, 0))
         @closed = false
-        @buffer = Pointer(T).null
         @wl_buffer = Pointer(LibWaylandClient::WlBuffer).null
-        @x_size = @y_size = @size = 0
+        @x_size = @y_size = 0
         @buffer_listener = LibWaylandClient::WlBufferListener.new(release: release)
+
+        @size = 0
+        @fd = IO::FileDescriptor.new(LibC.memfd_create("buffer".to_unsafe, 0))
+        @buffer = Pointer(T).null
       end
 
       private def release
@@ -32,20 +34,33 @@ module WaylandClient
       end
 
       def resize(x, y)
-        stride = x * pixel_size
-        new_size = stride * y
-        unmap if @wl_buffer
-        LibC.ftruncate(fd.fd, new_size)
-        @buffer = LibC.mmap(nil, new_size, LibC::PROT_READ | LibC::PROT_WRITE, LibC::MAP_SHARED, fd.fd, 0).as(Pointer(T))
-        pool = WaylandClient::LibWaylandClient.wl_shm_create_pool(shm, fd.fd, new_size)
-        @wl_buffer = WaylandClient::LibWaylandClient.wl_shm_pool_create_buffer(
-          pool, 0, x, y, stride, T.shm_format
-        )
-        WaylandClient::LibWaylandClient.wl_buffer_add_listener(@wl_buffer, pointerof(@buffer_listener), self.as(Void*))
-        WaylandClient::LibWaylandClient.wl_shm_pool_destroy(pool)
-        @size = new_size
+        destroy_buffer if @wl_buffer
+        @wl_buffer = allocate_buffer(x, y)
         @x_size = x - 1
         @y_size = y - 1
+      end
+
+      def allocate_buffer(x, y)
+        stride = x * pixel_size
+        new_size = stride * y
+        with_pool(new_size) do |pool|
+          wl_buffer = WaylandClient::LibWaylandClient.wl_shm_pool_create_buffer(
+            pool, 0, x, y, stride, T.shm_format
+          )
+          WaylandClient::LibWaylandClient.wl_buffer_add_listener(wl_buffer, pointerof(@buffer_listener), self.as(Void*))
+          wl_buffer
+        end
+      end
+
+      private def with_pool(requested_size)
+        unmap_pool if @wl_buffer
+        LibC.ftruncate(fd.fd, requested_size)
+        @buffer = LibC.mmap(nil, requested_size, LibC::PROT_READ | LibC::PROT_WRITE, LibC::MAP_SHARED, fd.fd, 0).as(Pointer(T))
+        shm_pool = WaylandClient::LibWaylandClient.wl_shm_create_pool(shm, fd.fd, requested_size)
+        @size = requested_size
+        wl_buffer = yield shm_pool
+        WaylandClient::LibWaylandClient.wl_shm_pool_destroy(shm_pool)
+        wl_buffer
       end
 
       def to_slice
@@ -79,7 +94,15 @@ module WaylandClient
       end
 
       def unmap
+        destroy_buffer
+        unmap_pool
+      end
+
+      private def destroy_buffer
         WaylandClient::LibWaylandClient.wl_buffer_destroy(@wl_buffer)
+      end
+
+      private def unmap_pool
         LibC.munmap(@buffer, @size)
       end
 
