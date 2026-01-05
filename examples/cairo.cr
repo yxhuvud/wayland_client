f6ca3e35cc05a42eec228ab2101dd08684b67dc6
Linus Sellberg <sellberg@gmail.com>
2026-01-05 17:41:05 +0100

    Cairo helper: make helper more configurable

--- examples/cairo.cr
+++ examples/cairo.cr
@@ -3,16 +3,20 @@ require "cairo"
 module Cairo
   # Note: This is probably not a good way to use Cairo. If I properly understand what is going on then it may be
   # wasteful to set up surface and contexts for each frame text is wanted.
-  def self.write_to_buf(buf, width, height, text)
+  def self.write_to_buf(buf, width, height, text,
+                        rgba = {0.0, 1.0, 1.0, 1.0},
+                        font_size = 10.0,
+                        pos = {15.0, 15.0},
+                        font_face = {"Sans", Cairo::FontSlant::Normal, Cairo::FontWeight::Normal})
     bytes = buf.to_unsafe_bytes
     format = Cairo::Format::ARGB32
     stride = format.stride_for_width(width)
     surface = Cairo::Surface.new(bytes, format, width, height, stride)
     context = Cairo::Context.new(surface)
-    context.set_source_rgba(0, 1, 1, 1)
-    context.font_size = 10.0
-    context.move_to(15, 15)
-    context.select_font_face("Sans", Cairo::FontSlant::Normal, Cairo::FontWeight::Normal)
+    context.set_source_rgba(*rgba)
+    context.font_size = font_size
+    context.move_to(*pos)
+    context.select_font_face(*font_face)
     context.show_text(text)
   end
 end
